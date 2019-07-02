2b0026b0730645f623f3791a4e2860fb262cdfc9
Eduard Toloza <tolosaeduard@gmail.com>
2019-07-01 23:07:17 -0500

    Added support for sending notifications.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::process::Command;
-extern crate schedule_recv;
+//extern crate schedule_recv;
+use notify_rust::Notification;
 
 fn main() {
     // Get user arguments, first argument is the level of battery
@@ -30,9 +31,14 @@ fn main() {
         // Execution time need to be miliseconds
         let execution_time = execution_time * 1000;
         let tick = schedule_recv::periodic_ms(execution_time);
+        let mut notification_status = String::from("Not runned");
         loop {
             tick.recv().unwrap();
             battery_critical_action(bat_discharging_action, &args[3]);
+            while notification_status == "Not runned" {
+                notification_status = battery_notification();
+                break;
+            }
         }
     } else {
         println!("Usage:
@@ -86,6 +92,7 @@ fn battery_critical_action(percentage: u32, action: &str) {
                 .arg(&action)
                 .output()
                 .expect("Failed to execute command.");
+
             if command_action.status.success() == false {
                 eprintln!("Process exited with: {}", &command_action.status);
                 eprintln!(
@@ -94,5 +101,70 @@ fn battery_critical_action(percentage: u32, action: &str) {
                 );
             }
         }
+    }
+}
+
+fn battery_notification() -> String {
+    // Path for battery capacity percentage, battery charging status and systemd
+    let battery_capacity_path = "/sys/class/power_supply/BAT0/capacity";
+    let battery_charging_path = "/sys/class/power_supply/BAT0/status";
+
+    let percentage_low = 20;
+    let percentage_crit = 10;
+
+    // Check if systemd and BAT0 device exist
+    if Path::new(&battery_capacity_path).exists() && Path::new(&battery_charging_path).exists() {
+        // Declare variables
+        let mut bat_value = String::new();
+        let mut bat_charging = String::new();
+
+        // Read the capacity from file
+        let mut b_capacity = File::open(&battery_capacity_path).expect("Error opening file.");
+        let mut b_status = File::open(&battery_charging_path).expect("Error opening file.");
+
+        // Convert percentage into string
+        b_capacity
+            .read_to_string(&mut bat_value)
+            .expect("Failed to read file.");
+        // Convert status into string
+        b_status
+            .read_to_string(&mut bat_charging)
+            .expect("Failed to read file.");
+
+        // Convert percentage into integer
+        let bat_percentage: u32 = bat_value
+            .trim()
+            .parse()
+            .expect("Error, battery percentage level can not be converted in a valid number.");
+
+        // Compare percentagei, status and execute actions accordy to use input
+        if bat_percentage <= percentage_low
+            && bat_percentage > percentage_crit
+            && bat_charging.trim() == "Discharging"
+        {
+            Notification::new()
+                .summary("Battery level low (20%)")
+                .action("Closed", "Close")
+                .body("Please consideer charging the battery.")
+                .icon("dialog-warning")
+                .timeout(0)
+                .show()
+                .unwrap();
+            String::from("Warning")
+        } else if bat_percentage <= percentage_crit && bat_charging.trim() == "Discharging" {
+            Notification::new()
+                .summary("Battery level critical (10%)")
+                .action("Closed", "Close")
+                .body("Please connect a charguer.")
+                .icon("dialog-error")
+                .timeout(0)
+                .show()
+                .unwrap();
+            String::from("Critical")
+        } else {
+            String::from("Not runned")
+        }
+    } else {
+        String::from("Some paths doesn't exist.")
     }
 }
